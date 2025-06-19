e7074141f958a2fe60d15bd8c33812f967dbec6b
Vedika Singh <vedikas2003@gmail.com>
2025-06-19 22:36:36 +0530

    Updated cloudinary-config

---
+++ apps/api/models/Product.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  subcategory: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: [String], 
+    required: false,
+    default: [],
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+}, {
+  timestamps: true,
+});
+
+export const Product = mongoose.model('Product', productSchema);
